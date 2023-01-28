ec9808ec4e39f6a32e218f8d1fd3e43aff78a01a
Ranjeet Kumar <ranjeet93ks@gmail.com>
2023-01-28 22:47:39 +0530

    commit few files

--- src/main/java/com/learnJava/Main.java
+++ src/main/java/com/learnJava/Main.java
@@ -2,7 +2,6 @@ package com.learnJava;
 
 import com.learnJava.data.Student;
 import com.learnJava.data.StudentDataBase;
-
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
@@ -11,14 +10,12 @@ import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 public class Main {
-
      BinaryOperator<List<String>> binaryOperator = (a,b)-> {
         System.out.println("input 1 : "+a + "input 2 : " + b);
         return a;
     };
 
     public  void listToMap(){
-
         Map<String, List<String>> result1 = StudentDataBase.getAllStudents().stream().collect(
                 Collectors.toMap(Student::getName, Student::getActivities,binaryOperator));
         System.out.println("Result 1 : " + result1);
@@ -26,7 +23,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
         new Main().listToMap();
 
         LocalTime localTime = LocalTime.of(23,59);
