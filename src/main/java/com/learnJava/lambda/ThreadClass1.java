394ab30a2cad428edb404c33d011ff21a96d9232
Ranjeet Kumar <ranjeet93ks@gmail.com>
2023-02-04 22:30:51 +0530

    my commit

--- src/main/java/com/learnJava/lambda/ThreadClass1.java
+++ src/main/java/com/learnJava/lambda/ThreadClass1.java
@@ -3,7 +3,7 @@ package com.learnJava.lambda;
 public class ThreadClass1 extends Thread {
 	@Override
 	public void run() {
-		System.out.println("ThreadClass1 implements Thread "); 
+		System.out.println("ThreadClass1 implements Thread ");  
 
 	}
 }
